53d51b2a956d7115887230317968e272ed177d58
dynamique1 <bee4keys@gmail.com>
2024-04-28 22:52:54 +0100

    Export_to_mongodb

--- crawler_tutorial/crawler_tutorial/pipelines.py
+++ crawler_tutorial/crawler_tutorial/pipelines.py
@@ -7,7 +7,21 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import logging
+import pymongo
 
-class CrawlerTutorialPipeline:
+class MongodbPipeline:
+    collection_name = 'transcript'
+    def open_spider(self,spider):
+        self.client = pymongo.MongoClient("mongodb+srv://jimohabiodunn:Myheartbit@cluster0.5iyjkjt.mongodb.net/")
+        self.db = self.client['My_DataBase']
+
+    
+    def close_spider(self,spider):
+        self.client.close()
+
+
+    
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
--- crawler_tutorial/crawler_tutorial/spiders/transcript_crawler.py
+++ crawler_tutorial/crawler_tutorial/spiders/transcript_crawler.py
@@ -6,17 +6,26 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptCrawlerSpider(CrawlSpider):
     name = "transcript_crawler"
     allowed_domains = ["subslikescript.com"]
-    start_urls = ["https://subslikescript.com/movies_letter-X"]
+    # start_urls = ["https://subslikescript.com/movies_letter-X"]
     rules=(
         Rule(LinkExtractor(restrict_xpaths=("//ul[@class='scripts-list']/a")), callback="parse_item", follow=True),
-        Rule(LinkExtractor(restrict_xpaths=('(//a[@rel="next"])[1]'))),
+        Rule(LinkExtractor(restrict_xpaths=('(//a[@rel="next"])[1]')),process_request='set_user_agent'),
 )
+    user_agent  = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 
+    def start_requests(self):
+        yield scrapy.Request(url='https://subslikescript.com/movies_letter-X', headers={'user-agent':self.user_agent})
+
+    def set_user_agent(self,request,spider):
+        request.headers['User-Agent']=self.user_agent
+        return request
+        
     def parse_item(self, response):
         article = response.xpath('//article[@class="main-article"]')
         yield{
             "Title":article.xpath('./h1/text()').get(),
             "Plot": article.xpath('./p/text()').get(),
-            # "Transcript":article.xpath('./div/text()').getall(),
+            "Transcript":article.xpath('./div/text()').getall(),
             "Url":response.url,
+            # "user-agent":response.request.headers['User-Agent']
         }
